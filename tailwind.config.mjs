2ed0128a921da0e7e1e71c849480f268eb1e2fc0
NutMontree <nutmontree29@gmail.com>
2025-06-17 12:14:54 +0700

    'v1'

---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./api/**/*.{js,ts,jsx,tsx,mdx}",
+    "./api/inngest/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      gridTemplateColumns: {
+        'auto': 'repeat(auto-fit, minmax(200px, 1fr))'
+      },
+    },
+  },
+  plugins: [],
+};
