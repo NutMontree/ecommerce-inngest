8d1e92497096219605b941beeb30c62cb825cd6b
NutMontree <nutmontree29@gmail.com>
2025-06-18 16:53:45 +0700

    'v1'

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -15,8 +15,8 @@ export async function POST(request) {
 
         // calculat amount using items
         const amount = await items.reduce(async (acc, item) => {
-            const product = await Product.findById(item, product);
-            return acc + product.offerPrice * item.quantity
+            const product = await Product.findById(item.product);
+            return await acc + product.offerPrice * item.quantity;
         }, 0)
 
         await inngest.send({
