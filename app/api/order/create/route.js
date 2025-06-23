b86e513ea5aac0d9fa5e9127b407bd75cb8e4dca
NutMontree <nutmontree29@gmail.com>
2025-06-23 13:30:11 +0700

    'v1'

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -6,42 +6,91 @@ import { NextResponse } from "next/server";
 
 export async function POST(request) {
     try {
-        const { userId } = getAuth(request);
-        const { address, items } = await request.json();
+        const { userId } = getAuth(request)
+        const { address, items } = await request.json()
 
         if (!address || items.length === 0) {
             return NextResponse.json({ success: false, message: "Invalid data" });
         }
 
-        // ดึงราคาสินค้าและคำนวณยอดรวม
-        const amountArray = await Promise.all(
-            items.map(async (item) => {
-                const product = await Product.findById(item.product);
-                return product.offerPrice * item.quantity;
-            })
-        );
-
-        const amount = amountArray.reduce((acc, val) => acc + val, 0);
+        // calculat amount using items
+        const amount = await items.reduce(async (acc, item) => {
+            const product = await Product.findById(item.product);
+            return await acc + product.offerPrice * item.quantity;
+        }, 0)
 
         await inngest.send({
-            name: "order/created",
+            name: 'order/created',
             data: {
                 userId,
                 address,
                 items,
                 amount: amount + Math.floor(amount * 0.02),
-                date: Date.now(),
-            },
-        });
+                date: Date.now()
+            }
+        })
 
-        // ล้างตะกร้าสินค้าผู้ใช้
-        const user = await User.findById(userId);
-        user.cartItems = {};
-        await user.save();
 
-        return NextResponse.json({ success: true, message: "Order Placed" });
+        // clear user cart
+        const user = await User.findById(userId)
+        user.cartItems = {}
+        await user.save()
+
+        return NextResponse.json({ success: true, message: 'Order Placed' })
+
     } catch (error) {
-        console.log(error);
-        return NextResponse.json({ success: false, message: error.message });
+        console.log(error)
+        return NextResponse.json({ success: false, message: error.message })
     }
 }
+
+
+
+
+// import { inngest } from "@/config/inngest";
+// import Product from "@/models/Product";
+// import User from "@/models/User";
+// import { getAuth } from "@clerk/nextjs/server";
+// import { NextResponse } from "next/server";
+
+// export async function POST(request) {
+//     try {
+//         const { userId } = getAuth(request);
+//         const { address, items } = await request.json();
+
+//         if (!address || items.length === 0) {
+//             return NextResponse.json({ success: false, message: "Invalid data" });
+//         }
+
+//         // ดึงราคาสินค้าและคำนวณยอดรวม
+//         const amountArray = await Promise.all(
+//             items.map(async (item) => {
+//                 const product = await Product.findById(item.product);
+//                 return product.offerPrice * item.quantity;
+//             })
+//         );
+
+//         const amount = amountArray.reduce((acc, val) => acc + val, 0);
+
+//         await inngest.send({
+//             name: "order/created",
+//             data: {
+//                 userId,
+//                 address,
+//                 items,
+//                 amount: amount + Math.floor(amount * 0.02),
+//                 date: Date.now(),
+//             },
+//         });
+
+//         // ล้างตะกร้าสินค้าผู้ใช้
+//         const user = await User.findById(userId);
+//         user.cartItems = {};
+//         await user.save();
+
+//         return NextResponse.json({ success: true, message: "Order Placed" });
+//     } catch (error) {
+//         console.log(error);
+//         return NextResponse.json({ success: false, message: error.message });
+//     }
+// }
